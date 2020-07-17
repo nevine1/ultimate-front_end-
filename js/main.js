1917641ba1288bd4c434fceec78b00191060afa6
Nevine <venasamaan@gmail.com>
2020-07-17 12:26:44 -0400

    lesson 13, 14, 15, 16, 17

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(function () {
   $(".toggle-sidebar").on("click", function () {
     $(".sidebar, .content-area").toggleClass("no-sidebar");
   });
+  // clicking on > to show the sidebar submenu
+  $(".toggle-submenu").on("click", function () {
+    $(this).find(".fa-chevron-right").toggleClass("down");
+    $(this).next(".child-links").slideToggle();
+  });
+
+  $(".main-links").click(function () {
+    $(".child-links").slideToggle();
+  });
 });
