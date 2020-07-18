f7dea2d1f2a55403a6ff2e971e868f93497d43b4
Nevine <venasamaan@gmail.com>
2020-07-17 21:03:32 -0400

    lesson 18, but eixt full screen function is not working

--- js/main.js
+++ js/main.js
@@ -13,4 +13,44 @@ $(function () {
   $(".main-links").click(function () {
     $(".child-links").slideToggle();
   });
+
+  //open & close fullscreen window
+  $(".toggle-fullscreen").on("click", function () {
+    $(this).toggleClass("full-screen");
+
+    if ($(this).hasClass("full-screen")) {
+      openFullscreen();
+    } else {
+      closeFullscreen();
+    }
+  });
 });
+
+var elem = document.documentElement;
+function openFullscreen() {
+  //this funciton is not working
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (elem.exitFullscreen) {
+    elem.exitFullscreen();
+  } else if (elem.webkitExitFullscreen) {
+    elem.webkitExitFullscreen();
+  } else if (elem.mozCancelFullScreen) {
+    elem.mozCancelFullScreen();
+  } else if (elem.msExitFullscreen) {
+    elem.msExitFullscreen();
+  }
+}
